5fc49fea964279a76fba5e1fe0f22de3c6da847b
theviktori39 <theviktori39@gmail.com>
2023-10-04 17:23:59 +0300

    Создан код на основе типов данных: byte, bool и double.

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -4,10 +4,27 @@
     {
         static void Main(string[] args)
         {
-            string MyName = "Victoria";
+            string MyName = "My name is Jane";
+            byte Myage = 27;
+            bool HaveIApet = true;
+            double MySchoeSize = 37.5;
+
             Console.WriteLine(MyName);
+            Console.WriteLine("My age is " + Myage);
+            Console.WriteLine("Do I have a pet? " + HaveIApet);
+            Console.WriteLine("My shoe size is " + MySchoeSize);
+
+            
+
+            
+
            
 
+            
+
+            
+
+            
         }
 
     }
