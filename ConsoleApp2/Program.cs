05f2bfb2348868f74ae87b8ae2d8b21feb2d7039
theviktori39 <theviktori39@gmail.com>
2023-10-09 21:43:49 +0300

    Создан код финальной практики - "Анкетирование пользователей".

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -4,19 +4,34 @@ class MainClass
 {
     public static void Main(string[] args)
     {
-        ConsoleColor Semaphore;
+        Console.Write("What is your name?: ");
+        var name = Console.ReadLine();
+        Console.Write("How old are you?: ");
+        var age = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Your name is {0} and age is {1}", name, age); 
+        Console.Write("When's your birthday?: ");
+        var birthday = Console.ReadLine();
+        Console.WriteLine("Your birthday {0} ", birthday);
+        Console.ReadKey();
 
-        Semaphore = ConsoleColor.Green;
 
-        Console.WriteLine(Semaphore);
+
+       
+
+
+
+
+
+
+     
+
+
+
+
+
+
+
+
     }
-}
-
-enum Semaphore
-{
-    Red = 100,
-    Yellow = 200,
-    Green = 300
-
 }
 
