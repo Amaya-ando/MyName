38592e3bf94544ae513d10a9b877e643f6f36031
theviktori39 <theviktori39@gmail.com>
2023-10-13 21:38:15 +0300

    Задание 4.1.17. Написан код - изменение цвета консоли. If. else if, else

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -2,28 +2,49 @@
 
 class MainClass
 {
-    public static void Main(string[] args)
-    {
-        Console.Write("What is your name?: ");
-        var name = Console.ReadLine();
-        Console.Write("How old are you?: ");
-        var age = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine("Your name is {0} and age is {1}", name, age); 
-        Console.Write("When's your birthday?: ");
-        var birthday = Console.ReadLine();
-        Console.WriteLine("Your birthday {0} ", birthday);
-        Console.ReadKey();
+    public static void Main(string[] args) {
+        Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
 
+        var color = Console.ReadLine();
+        
+        if (color == "red")
+        {
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.ForegroundColor = ConsoleColor.Black;
 
+            Console.WriteLine("Your color is red!");
+        }
+     
+       else if (color == "green")
+        {
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.Black;
 
+            Console.WriteLine("Your color is green!");
+        }
        
 
+        else
+        {
+            Console.BackgroundColor = ConsoleColor.Cyan;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine("Your cplpr is cyan!");
+        }
 
 
 
 
 
-     
+
+
+
+
+
+
+
+
+
+
 
 
 
