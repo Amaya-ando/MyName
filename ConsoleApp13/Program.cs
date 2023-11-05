a1391a589ddcd606e635cb418a34f3754a5fd8d5
theviktori39 <theviktori39@gmail.com>
2023-11-05 20:49:35 +0300

    Код - "анкетирование пользователя" - кортеж.

--- ConsoleApp13/Program.cs
+++ ConsoleApp13/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Globalization;
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
 
 namespace ConsoleApp13
 {
@@ -7,31 +8,35 @@ namespace ConsoleApp13
         static void Main(string[] args)
         {
 
-            var arr = new int[] { 5, 6, 9, 2, 3, 1, 4 };
-                               //
-            int sum = 0;
-            for (int i = 0; i < arr.Length; i++)
-            {
-                
-                sum += arr[i];
-                  
-            }
-            Console.WriteLine(sum);
-            
-            
+            int[][] array = new int[3][];
+            array[0] = new int[2] { 1, 2 };
+            array[1] = new int[3] { 1, 2, 3 };
+            array[2] = new int[5] { 1, 2, 3, 4, 5 };
 
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-                
+            foreach (var num in array)
+            {
+                foreach (var item in num)
+                {
+                    Console.Write(item + " ");
+                }
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            (string name, int age) anketa;
+
+            Console.Write("Ваше имя: ");
+            anketa.name = Console.ReadLine();
+            Console.Write("Ваш возраст: ");
+            anketa.age = Convert.ToInt32(Console.ReadLine());   
+
+            Console.WriteLine("Ваше имя: {0}", anketa.name);
+            Console.WriteLine("Вас зовут: {0}", anketa.age);
+
+            Console.ReadKey();
+            
+        }
+
+          
+
+
+
+
+
+
+
+
+
+        }
+        
+        
+
+       
+    }
